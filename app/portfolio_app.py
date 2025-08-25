7dd7a978463165decc6a15813a51263439e885ad
HARSHAL PAREKH <harahalparekh40@gmail.com>
2025-08-25 11:13:11 +0530

    Fix import path using ROOT_DIR in portfolio_app.py

--- app/portfolio_app.py
+++ app/portfolio_app.py
@@ -1,4 +1,7 @@
 import sys, os
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import streamlit as st
 import os
